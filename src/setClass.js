2c24f9731b5ddb578ea4e0aec32368fec278bed2
Alex Barth <AlexBarthGlobal@gmail.com>
2021-12-19 23:58:43 -0500

    Basic selection date logic and styling is finished

--- src/setClass.js
+++ src/setClass.js
@@ -1,10 +1,11 @@
-import monthInfo from './monthInfo';
-
 const setClass = (currDate, startDate, endDate) => {
-    if (currDate === startDate || currDate === endDate) return 'selectedDate';
-    else if (endDate && startDate < currDate && currDate < endDate) return 'betweenDate';
+    // if (currDate === startDate || currDate === endDate) return 'selectedDate';
+    if (currDate === startDate) {
+        if (!endDate) return ['selectedDate'];
+        else return ['selectedDate', 'startDateBackground']
+    } else if (currDate === endDate) return ['selectedDate', 'endDateBackground']
+    else if (endDate && startDate < currDate && currDate < endDate) return ['betweenDate'];
+    else return [];
 };
-
-
 
 export default setClass
