14d1c380e6e9489d63a08f2a3d413ea06cd9322d
Alex Barth <AlexBarthGlobal@gmail.com>
2021-12-20 02:48:10 -0500

    AddedIcons

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import monthInfo from './monthInfo';
 import generateDays from './generateDays'
+import {ReactComponent as ArrowLeft} from './icons/ArrowLeft.svg'
+import {ReactComponent as ArrowRight} from './icons/ArrowRight.svg'
 
 const Calendar = (props) => {
     // useMemo for generateDays, don't need to load that on each re-render
     return (
         <div className='calendar'>
             <div className='calendarHead'>
-                {!props.side || props.side === 'L' ? <button className='changeMonth' onClick={() => props.decrementMonth()}>{'<-'}</button> : <div className='calHeadSpace'></div>}
+                {!props.side || props.side === 'L' ? <ArrowLeft className='changeMonth' onClick={() => props.decrementMonth()} /> : <div className='calHeadSpace'></div>}
                 <div>{`${monthInfo[props.month][0]} ${props.year}`}</div>
-                {!props.side || props.side === 'R' ? <button className='changeMonth' onClick={() => props.incrementMonth()}>{'->'}</button> : <div className='calHeadSpace'></div>}
+                {!props.side || props.side === 'R' ? <ArrowRight className='changeMonth' onClick={() => props.incrementMonth()} /> : <div className='calHeadSpace'></div>}
             </div>
             <table className='calendarTable'>
                 <thead>
